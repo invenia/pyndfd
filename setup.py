286df97a6b6db5e9a4f241dfb6b181f7c7fa5fa6
Fernando Chorney <fernando.chorney@invenia.ca>
2018-10-19 16:57:50 -0500

    Initial conversion
    - py files ran through iSort and Black
    - All flake8 errors fixed
    - Some python2/3 compatability fixes
    - Use pyproject.toml to build numpy before building project as numpy
    needs to be fully installed into the environment before setup.py can do
    its thing.
    - Add a tox.ini file with a few configs
    - demo.py currently runs properly under python 2 and 3.

--- setup.py
+++ setup.py
@@ -1,15 +1,31 @@
-from distutils.core import setup
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from setuptools import find_packages, setup
+
+TEST_DEPS = ["coverage", "pytest", "pytest-cov"]
+EXTRAS = {"test": TEST_DEPS}
 
 setup(
-  name = 'pyndfd',
-  packages = ['pyndfd'],
-  version = '0.9',
-  license = 'MIT License',
-  description = 'Python routines for easy caching/retrieval of NWS\'s NDFD variables',
-  author = 'Marty J. Sullivan',
-  author_email = 'marty.sullivan@cornell.edu',
-  url = 'https://github.com/marty-sullivan/pyndfd',
-  download_url = 'https://github.com/marty-sullivan/pyndfd/archive/0.9.tar.gz',
-  install_requires = ['numpy', 'pyproj', 'pygrib'],
-  keywords = ['noaa', 'ndfd', 'nws', 'weather', 'forecast', 'cornell', 'atmospheric', 'science']
+    name="pyndfd",
+    packages=find_packages(exclude=["tests"]),
+    version="0.9",
+    license="MIT License",
+    description="Python routines for easy caching/retrieval of NWS's NDFD variables",
+    author="Marty J. Sullivan",
+    author_email="marty.sullivan@cornell.edu",
+    url="https://github.com/marty-sullivan/pyndfd",
+    download_url="https://github.com/marty-sullivan/pyndfd/archive/0.9.tar.gz",
+    install_requires=["numpy", "pyproj", "pygrib", "six"],
+    test_require=TEST_DEPS,
+    extras_require=EXTRAS,
+    keywords=[
+        "noaa",
+        "ndfd",
+        "nws",
+        "weather",
+        "forecast",
+        "cornell",
+        "atmospheric",
+        "science",
+    ],
 )
